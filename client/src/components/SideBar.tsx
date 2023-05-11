a2c6b74be4b0ebfb9a0bc482df4024cc9fb2379d
QuangAnh22112003 <quanganhbh1@gmail.com>
2023-05-11 23:26:51 +0700

    get task

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -1,66 +1,78 @@
 import { IoMdNotificationsOutline } from "react-icons/io";
 import TimeLine from "./TimeLine";
 import Task from "../types/Tasks";
+import { useEffect, useState } from "react";
+import request from "../utils/request";
 
-const todo: Task[] = [
-  {
-    id: 10,
-    started_time: new Date("1995-12-17T03:20:00"),
-    finished_time: new Date("1995-12-17T03:20:00"),
-    task_name: "Project X",
-    title: "Brainstorming",
-    description:
-      "An existing Date object. This effectively makes a copy of the existing Date object with the same date and time. An existing Date object. This effectively makes a copy of the existing Date object with the same date and time. An existing Date object. This effectively makes a copy of the existing Date object with the same date and time. An existing Date object. This effectively makes a copy of the existing Date object with the same date and time.",
-    priority: "HIGH",
-    status: "INPROGRESS",
-  },
-  {
-    id: 11,
-    started_time: new Date("1995-01-17T03:24:00"),
-    finished_time: new Date("1995-01-17T03:24:00"),
-    task_name: "Project Y",
-    title: "Brainstorming",
-    description:
-      "When no parameters are provided, the newly-created Date object represents the current date and time as of the time of instantiation.",
-    priority: "LOW",
-    status: "COMPLETED",
-  },
-  {
-    id: 12,
-    started_time: new Date("1995-07-31T03:25:00"),
-    finished_time: new Date("1995-07-31T03:25:00"),
-    task_name: "Project Z",
-    title: "Brainstorming",
-    description: "Utilities for controlling the font size of an element.",
-    priority: "MEDIUM",
-    status: "STOPPED",
-  },
-  {
-    id: 13,
-    started_time: new Date("1995-07-31T03:25:00"),
-    finished_time: new Date("1995-07-31T03:25:00"),
-    task_name: "Project W",
-    title: "Brainstorming",
-    description: "Utilities for controlling the font size of an element.",
-    priority: "HIGH",
-    status: "INPROGRESS",
-  },
-  {
-    id: 14,
-    started_time: new Date("1995-07-31T03:25:00"),
-    finished_time: new Date("1995-07-31T03:25:00"),
-    task_name: "Project Z",
-    title: "Brainstorming",
-    description: "Utilities for controlling the font size of an element.",
-    priority: "LOW",
-    status: "COMPLETED",
-  },
-];
+// const todo: Task[] = [
+//   {
+//     id: 10,
+//     started_time: new Date("1995-12-17T03:20:00"),
+//     finished_time: new Date("1995-12-17T03:20:00"),
+//     task_name: "Project X",
+//     title: "Brainstorming",
+//     description:
+//       "An existing Date object. This effectively makes a copy of the existing Date object with the same date and time. An existing Date object. This effectively makes a copy of the existing Date object with the same date and time. An existing Date object. This effectively makes a copy of the existing Date object with the same date and time. An existing Date object. This effectively makes a copy of the existing Date object with the same date and time.",
+//     priority: "HIGH",
+//     status: "INPROGRESS",
+//   },
+//   {
+//     id: 11,
+//     started_time: new Date("1995-01-17T03:24:00"),
+//     finished_time: new Date("1995-01-17T03:24:00"),
+//     task_name: "Project Y",
+//     title: "Brainstorming",
+//     description:
+//       "When no parameters are provided, the newly-created Date object represents the current date and time as of the time of instantiation.",
+//     priority: "LOW",
+//     status: "COMPLETED",
+//   },
+//   {
+//     id: 12,
+//     started_time: new Date("1995-07-31T03:25:00"),
+//     finished_time: new Date("1995-07-31T03:25:00"),
+//     task_name: "Project Z",
+//     title: "Brainstorming",
+//     description: "Utilities for controlling the font size of an element.",
+//     priority: "MEDIUM",
+//     status: "STOPPED",
+//   },
+//   {
+//     id: 13,
+//     started_time: new Date("1995-07-31T03:25:00"),
+//     finished_time: new Date("1995-07-31T03:25:00"),
+//     task_name: "Project W",
+//     title: "Brainstorming",
+//     description: "Utilities for controlling the font size of an element.",
+//     priority: "HIGH",
+//     status: "INPROGRESS",
+//   },
+//   {
+//     id: 14,
+//     started_time: new Date("1995-07-31T03:25:00"),
+//     finished_time: new Date("1995-07-31T03:25:00"),
+//     task_name: "Project Z",
+//     title: "Brainstorming",
+//     description: "Utilities for controlling the font size of an element.",
+//     priority: "LOW",
+//     status: "COMPLETED",
+//   },
+// ];
 
 function SideBar() {
   // Query data
   // TODO:
   // const todo: Task[] = ...
+  const [todo, setTodo] = useState<Task[]>();
+  useEffect(() => {
+    request.get<Task[]>('task/today')
+      .then(response => {
+        setTodo(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
   return (
     <div className="border-[1.5px] rounded-md m-3 font-poppins">
--- server/src/routes/taskRouter.js
+++ server/src/routes/taskRouter.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post('/', taskController.taskPost);
 router.get('/', taskController.getTask);
+router.get('/today', taskController.getTodayTask);
 router.delete('/delete/:task_id', taskController.deleteTask);
 router.post('/update', taskController.updateTask);
 
