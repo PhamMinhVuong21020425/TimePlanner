1b63f5901c4acd4bf923f6d645af81bbaf3f54ef
PhamMinhVuong <100543300+PhamMinhVuong21020425@users.noreply.github.com>
2023-08-31 10:25:58 +0700

    Fix bug

--- client/src/utils/request.ts
+++ client/src/utils/request.ts
@@ -3,9 +3,6 @@ import axios from 'axios';
 const request = axios.create({
     baseURL: 'https://time-planner-server-deploy.vercel.app/',
     withCredentials: true, // Cho phép server gửi cookie
-    headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-    }
 });
 
 export default request;
